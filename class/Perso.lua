0b3267394ab8c6a42b11873147134664d178bd10
Antoine DOUSSAUD <antoine@doussaud.org>
2014-07-19 22:11:41 +0200

    test

--- class/Perso.lua
+++ class/Perso.lua
@@ -3,18 +3,49 @@ local Sprite 	= require 'class/Sprite'
 
 local Perso = class('Perso')
 
-function Perso:initialize(posX, posY)
+function Perso:initialize()
   	
-  	self.posX = posX or 32
-  	self.posY = posY or 32
-	self.sprite = Sprite:new("texture/sprite.png", 32, 32)
+  	self.x =  32
+  	self.y =  32
+	
+	self.dX = 0
+	self.dY = 0
+	self.test = Sprite:new("texture/sprite.png", 32, 32)
 
 end
 
 function Perso:draw()
-	
-	self.sprite:draw(self.posX, self.posY)
 
+	--love.graphics.rectangle( "fill", self.posX, self.posY, 32, 32)
+	print(self.x)
+	--self.test:drawframe(self.posX, self.posY, 1)
+
+end
+
+function Perso:update(dt)
+	
+	--self.sprite:update(dt)
+
+	self.posX = self.posX + (self.dX * dt)
+	self.posY = self.posY + (self.dY * dt)
+	
+end
+
+
+function Perso:up()
+	self.dY = -1;
+end
+
+function Perso:down()
+	self.dY = 1;
+end
+
+function Perso:left()
+	self.dX = -1;
+end
+
+function Perso:right()
+	self.dX = 1;
 end
 
 return Perso
--- main.lua
+++ main.lua
@@ -14,14 +14,18 @@ local Perso = require "class/Perso"
 local Map 	= require "class/Map"
 
 function love.load()
-	map 	= Map:new("map/exemple.json","map/tileset.png")
+	map 	= Map:new("map/MapBomberMan..json","map/tileset.png")
 	perso 	= Perso:new(32,32);
 end
 
 
 function love.draw()
 	map:draw(0,0)
-	perso:draw()
+	Perso:draw()
+end
+
+function love.update(dt)
+	--perso:update(dt)
 end
 
 function love.mousepressed(x,y,button)
